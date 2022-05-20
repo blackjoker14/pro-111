2b7dfa79a47b749ca80636353245888aecac4be3
blackjoker14 <86548199+blackjoker14@users.noreply.github.com>
2022-05-20 17:54:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality : 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+//Capture image
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>';
+    })
+}
+
+//Code of gesture identifier
+
+console.log('ml5 version : ', ml5.version);
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/Ys_rwznEk/model.json", modelLoaded);
+
+function modelLoaded(){
+    console.log('Model Loaded!');
+}
+
+function speak(){
+    var synth = window.speakSynthesis;
+    speak_data_1 = "First prediction is" + prediction_1;
+    speak_data_2 = "Second prediction is" + prediction_2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
